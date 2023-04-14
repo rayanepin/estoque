9edb99c386f5fcc379dc7eb376279605cc27103b
rayanepin <rayanee.marques@gmail.com>
2023-04-14 17:29:50 -0400

    Applying product search strategy

--- app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
+++ app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
@@ -1,5 +1,6 @@
 package br.com.alura.estoque.ui.activity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -49,24 +50,34 @@ public class ListaProdutosActivity extends AppCompatActivity {
         ProdutoService service = new EstoqueRetrofit().getProdutoService();
         Call<List<Produto>> call = service.buscaTodos();
 
-        new BaseAsyncTask<>(() -> {
-            try {
-                Response<List<Produto>> resposta = call.execute();
-                List<Produto> produtosNovos = resposta.body();
-                dao.salva(produtosNovos);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return dao.buscaTodos();
-        }, produtosNovos -> {
-            if (produtosNovos != null) {
-                adapter.atualiza(produtosNovos);
-            } else {
-                Toast.makeText(this,
-                        "Não foi possóvel buscar os produtos da API.",
-                        Toast.LENGTH_SHORT).show();
-            }
-        }).execute();
+        new BaseAsyncTask<>(dao::buscaTodos,
+                resultado -> {
+                    adapter.atualiza(resultado);
+                    new BaseAsyncTask<>(() -> {
+                        try {
+                            try {
+                                Thread.sleep(5000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            Response<List<Produto>> resposta = call.execute();
+                            List<Produto> produtosNovos = resposta.body();
+                            dao.salva(produtosNovos);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return dao.buscaTodos();
+                    }, produtosNovos -> {
+                        if (produtosNovos != null) {
+                            adapter.atualiza(produtosNovos);
+                        } else {
+                            Toast.makeText(this,
+                                    "Não foi possóvel buscar os produtos da API.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                })
+                .execute();
     }
 
     private void configuraListaProdutos() {
