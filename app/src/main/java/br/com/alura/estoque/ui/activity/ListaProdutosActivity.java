15d03acc60bd28decd73f398151b1293cc09150e
rayanepin <rayanee.marques@gmail.com>
2023-04-16 13:39:21 -0400

    Refactoring the product search code

--- app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
+++ app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
@@ -2,12 +2,11 @@ package br.com.alura.estoque.ui.activity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.IOException;
 import java.util.List;
@@ -47,37 +46,34 @@ public class ListaProdutosActivity extends AppCompatActivity {
     }
 
     private void buscaProdutos() {
-        ProdutoService service = new EstoqueRetrofit().getProdutoService();
-        Call<List<Produto>> call = service.buscaTodos();
+        buscaProdutosInternos();
+    }
 
+    private void buscaProdutosInternos() {
         new BaseAsyncTask<>(dao::buscaTodos,
                 resultado -> {
                     adapter.atualiza(resultado);
-                    new BaseAsyncTask<>(() -> {
-                        try {
-                            try {
-                                Thread.sleep(5000);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            Response<List<Produto>> resposta = call.execute();
-                            List<Produto> produtosNovos = resposta.body();
-                            dao.salva(produtosNovos);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        return dao.buscaTodos();
-                    }, produtosNovos -> {
-                        if (produtosNovos != null) {
-                            adapter.atualiza(produtosNovos);
-                        } else {
-                            Toast.makeText(this,
-                                    "Não foi possóvel buscar os produtos da API.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                    buscaProdutosNaApi();
                 })
                 .execute();
+    }
+
+    private void buscaProdutosNaApi() {
+        ProdutoService service = new EstoqueRetrofit().getProdutoService();
+        Call<List<Produto>> call = service.buscaTodos();
+
+        new BaseAsyncTask<>(() -> {
+            try {
+                Response<List<Produto>> resposta = call.execute();
+                List<Produto> produtosNovos = resposta.body();
+                dao.salva(produtosNovos);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return dao.buscaTodos();
+        }, produtosNovos ->
+                adapter.atualiza(produtosNovos))
+                .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     private void configuraListaProdutos() {
